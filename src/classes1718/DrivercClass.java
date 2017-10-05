2add5addb2177fb66774fe966764e7822963a69e
PeterdHapke <phapke@marshalltown.k12.ia.us>
2017-10-05 15:53:22 -0500

    Updates to files and added text GUI

--- src/classes1718/DrivercClass.java
+++ src/classes1718/DrivercClass.java
@@ -1,9 +1,105 @@
 package classes1718;
 import java.util.List;
+import java.util.Scanner;
 
 public class DrivercClass {
 	
-	public static void main( String[ ] args ) {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		boolean runMenuAgain = true; 
+		while(runMenuAgain){
+			runMenuAgain = mainMenu(in); 
+		}
+		in.close();
+	}
+	
+	private static boolean mainMenu(Scanner in){
+		boolean runAgain = true; 
+		System.out.println();
+		System.out.print("************ Main Menu ************\n"
+					   + "Please choose an option to proceed\n"
+					   + "Enter 1 to - Add a student file\n"
+					   + "Enter 2 to - Get a student file\n"
+					   + "Enter 3 to - Update a student file\n"
+					   + "Enter 4 to - Delete a student file\n"
+					   + " - or Enter any other key to exit\n");
+		System.out.print("Choice: ");
+		
+		String choice = in.next();
+		
+		switch(choice) {
+			case "1": 
+				addAStudentFile(in); 
+			break; 
+			case "2": 
+				getAStudentFile(in);
+			break; 
+			case "3": 
+				updateAStudentFile(in);
+			break; 
+			case "4": 
+				deleteAStudentFile(in);
+			break; 
+			default: 
+				System.out.print("Goodbye!");
+				runAgain = false; 
+		};  
+		
+		return runAgain; 
+	}	
+		
+	private static void addAStudentFile(Scanner in){
+		System.out.println("This will create a student file");
+		System.out.println("Please enter the students first name");
+		System.out.print("First Name: ");
+		String fname = in.next();
+		System.out.println("Please enter the students last name");
+		System.out.print("Last Name: ");
+		String lname = in.next(); 
+		System.out.println("Please enter the students class period");
+		System.out.print("Class period: ");
+		int period = in.nextInt(); 
+		System.out.println("Please enter the students ID number");
+		System.out.print("ID: ");
+		int ID = in.nextInt();
+		CreateStudent.addStudent(fname, lname, period, ID);
+	}
+	
+	private static void getAStudentFile(Scanner in){
+		System.out.println("This will print a student file");
+		System.out.println("Please enter the students ID number");
+		System.out.print("ID: ");
+		int ID = in.nextInt();
+		Student student = GetStudent.fetchByID(ID);
+		System.out.println(student.toString());
+	}
+	
+	private static void updateAStudentFile(Scanner in){
+		System.out.println("This will update a student file");
+		System.out.println("Please enter the students ID number");
+		System.out.print("ID: ");
+		int ID = in.nextInt();
+		System.out.println("Please enter the students first name");
+		System.out.print("First Name: ");
+		String fname = in.next();
+		System.out.println("Please enter the students last name");
+		System.out.print("Last Name: ");
+		String lname = in.next(); 
+		System.out.println("Please enter the students class period");
+		System.out.print("Class period: ");
+		int period = in.nextInt(); 
+		UpdateStudent.update(new Student(fname, lname, period, ID));
+	}
+	
+	private static void deleteAStudentFile(Scanner in){
+		System.out.println("This will delete a student file");
+		System.out.println("Please enter the students ID number");
+		System.out.print("ID: ");
+		int ID = in.nextInt();
+		UpdateStudent.delete(ID);
+	}
+	
+	public static void testDatabase() {
 		//this section will test adding students to the database
 		CreateStudent.addStudent("Sage", "Blue", 2, 101001); 
 	
@@ -35,6 +131,17 @@ public class DrivercClass {
 		
 		//this section deletes a record
 		UpdateStudent.delete(101002);
+		
+		System.out.println("All tests returned postive, clearing the database"); 
+		
+		//this section will delete all the remaining records
+		
+		UpdateStudent.delete(101001);
+		UpdateStudent.delete(101003);
+		UpdateStudent.delete(101004);
+		UpdateStudent.delete(101005);
+		UpdateStudent.delete(101006);
+
 		
 		
 	}
